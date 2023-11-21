21eb03ec63642a3986838fde298cdd3f0e5e33a6
kingbookk <kingbooktashkent@gmail.com>
2023-11-21 17:23:33 +0500

    store

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL ="mongodb+srv://root:root@cluster0.g4zhvs8.mongodb.net/books-collections?retryWrites=true&w=majority"
+
+// mongodb+srv://root:<password>@cluster0.g4zhvs8.mongodb.net/?retryWrites=true&w=majority
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
